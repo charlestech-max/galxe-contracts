b19c875d17cefd2f64d55f1f6d48ed5c96e612e3
charlestech-max <charles.tech0x77@outlook.com>
2026-02-26 17:49:24 -0800

    Initial commit after re-initialization

---
+++ hardhat.config.ts
@@ -0,0 +1,81 @@
+import "@matterlabs/hardhat-zksync-verify";
+import "dotenv/config";
+import "hardhat-deploy";
+import "hardhat-gas-reporter";
+import type { HardhatUserConfig } from "hardhat/config";
+
+import { networks } from "./chains";
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  etherscan: {
+    apiKey: {
+      arbitrumOne: process.env.ARBISCAN_API_KEY || "",
+      avalanche: process.env.SNOWTRACE_API_KEY || "",
+      bsc: process.env.BSCSCAN_API_KEY || "",
+      bscTestnet: process.env.BSCSCAN_TESTNET_API_KEY || "",
+      mainnet: process.env.ETHERSCAN_API_KEY || "",
+      optimisticEthereum: process.env.OPTIMISM_API_KEY || "",
+      polygon: process.env.POLYGONSCAN_API_KEY || "",
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY || "",
+      sepolia: process.env.ETHERSCAN_API_KEY || "",
+      zkSyncEra: process.env.ETHERSCAN_API_KEY || "",
+    },
+  },
+
+  gasReporter: {
+    currency: "USD",
+    enabled: !!process.env.REPORT_GAS,
+    excludeContracts: [],
+    src: "./contracts",
+  },
+  networks: networks,
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
+  paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./tests",
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+        settings: {
+          metadata: {
+            bytecodeHash: "none",
+          },
+          optimizer: {
+            enabled: true,
+            runs: 800,
+          },
+        },
+      },
+      {
+        version: "0.7.6",
+        settings: {
+          metadata: {
+            bytecodeHash: "none",
+          },
+          optimizer: {
+            enabled: true,
+            runs: 800,
+          },
+        },
+      },
+    ],
+  },
+  typechain: {
+    // outDir: "types",
+    target: "ethers-v6",
+  },
+  sourcify: {
+    enabled: true,
+  },
+};
+
+export default config;
